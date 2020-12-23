4c826d69aa6b0eafebf52502cac6b9fe48d01cf0
Ben Winding <hello@benwinding.com>
2020-12-23 18:09:16 +1030

    Implemented --minimal flag Uses dockerfile deployment, much smaller footprint than herokuish

--- cli-implementation.js
+++ cli-implementation.js
@@ -14,8 +14,12 @@ commander
     "The dokku git url (eg: user@host:app)"
   )
   .requiredOption("-d, --dist <directory>", "The static directory")
+  .option(
+    "--minimal",
+    "Use minimal docker image instead of herokuish buildpack"
+  )
   .action((options) => {
-    const { giturl, dist } = options;
+    const { giturl, dist, minimal } = options;
     if (!checkIsGitUrl(giturl)) {
       console.log(chalk.red('--giturl must of the format "user@host:app"'));
       console.log(chalk.red("  example:    dokku@example.com:myapp"));
@@ -24,25 +28,32 @@ commander
     const distFull = path.resolve(dist);
     const tempPrefix = path.join(os.tmpdir(), "dokku-pages-");
     const tempDir = fs.mkdtempSync(tempPrefix);
+    const [dokkuUserHost, dokkuApp] = giturl.split(":");
 
-    console.log("Deploying giturl: " + giturl);
-    console.log("Deploying dist: " + distFull);
-    console.log("Deploying tempDir: " + tempDir);
+    console.log(
+      chalk.green(`============================
+ Deploying with Dokku Pages
+============================`)
+    );
+    console.log(
+      chalk.blue(`
+ dokku giturl = ${giturl}
+  dokku login = ${dokkuUserHost}
+    dokku app = ${dokkuApp}
+      app dir = ${distFull}
+     temp dir = ${tempDir}
+`)
+    );
 
-    sh.cp("-R", path.join(__dirname, "deploy-src", "*"), tempDir);
-    sh.cp("-R", path.join(distFull, "*"), path.join(tempDir, "public"));
-
-    function execTemp(cmd) {
-      sh.exec(cmd, { cwd: tempDir });
+    if (minimal) {
+      HandleMinimalDockerImage(dokkuUserHost, dokkuApp, tempDir);
+    } else {
+      HandleNormalStaticServer(dokkuUserHost, dokkuApp, tempDir);
     }
 
-    const remoteBranch = "dokku";
+    CopyDistToTempPublic(distFull, tempDir);
 
-    execTemp("git init");
-    execTemp("git add .");
-    execTemp('git commit -am "Deploy"');
-    execTemp(`git remote add ${remoteBranch} ${giturl}`);
-    execTemp(`git push -f ${remoteBranch} master`);
+    PushToDokku(giturl, tempDir);
 
     rimraf(tempDir, (err) => {
       if (err) {
@@ -50,6 +61,37 @@ commander
       }
     });
   });
+
+function PushToDokku(giturl, tempDir) {
+  function execTemp(cmd) {
+    sh.exec(cmd, { cwd: tempDir });
+  }
+
+  const remoteBranch = "dokku";
+
+  execTemp("git init");
+  execTemp("git add .");
+  execTemp('git commit -am "Deploy"');
+  execTemp(`git remote add ${remoteBranch} ${giturl}`);
+  execTemp(`git push -f ${remoteBranch} master`);
+}
+
+function HandleNormalStaticServer(dokkuUserHost, dokkuApp, tempDir) {
+  // const dokkuCmdSetMinimal = `ssh ${dokkuUserHost} config:set --no-restart ${dokkuApp} DOKKU_PROXY_PORT_MAP="http\\:80\\:5000\\ https\\:443\\:5000"`;
+  // sh.exec(dokkuCmdSetMinimal);
+  sh.cp("-R", path.join(__dirname, "deploy-src", "*"), tempDir);
+}
+
+function HandleMinimalDockerImage(dokkuUserHost, dokkuApp, tempDir) {
+  // const dokkuCmdSetMinimal = `ssh ${dokkuUserHost} config:set --no-restart ${dokkuApp} DOKKU_PROXY_PORT_MAP="http\\:80\\:5000\\ https\\:443\\:5000"`;
+  // const dokkuCmdSetMinimal = `ssh ${dokkuUserHost} config:unset --no-restart ${dokkuApp} DOKKU_PROXY_PORT_MAP`;
+  // sh.exec(dokkuCmdSetMinimal);
+  sh.cp("-R", path.join(__dirname, "deploy-minimal", "*"), tempDir);
+}
+
+function CopyDistToTempPublic(distFull, tempDir) {
+  sh.cp("-R", path.join(distFull, "*"), path.join(tempDir, "public"));
+}
 
 function checkIsGitUrl(giturl) {
   const result = /[\w-\.]*\@[\w-\.]*:[\w-\.]*/.exec(giturl);
